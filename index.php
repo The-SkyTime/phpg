2f818c3d99e4b5b08fb77bb590ebea82884240aa
sky-time <tim.wijn@gmail.com>
2019-01-25 11:26:26 +0100

    Update

--- index.php
+++ index.php
@@ -1,12 +1,41 @@
 <?php
 //Opdr 1
-echo "Hello world.";
+//echo "Hello world.";
 
 //Opdr 2
-$aantal = 14;
-echo $aantal;
+//$aantal = 14;
+//echo $aantal;
+//
+//$deze_jongen = "Deze jongen is ";
+//echo $deze_jongen;
+//
+//echo $deze_jongen.$aantal." jaar oud!";
 
-$deze_jongen = "Deze jongen is ";
-echo $deze_jongen;
+//Opdr 3
+$apen = array("chimpansee", "oerang oetang", "gorilla", "neusaap");
+echo $apen[2];
 
-echo $deze_jongen.$aantal." jaar oud!";
+$apen [4] = "mandril";
+
+$apen [6] = "baviaan";
+echo "<ul>";
+foreach ($apen as $aap) {
+    echo "<li>".aap."</li>";
+}
+echo "</ul>";
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PHP Opdrachten</title>
+</head>
+<body>
+<?php
+for ($i = 1; $i<=9; $i++) {
+    echo "<img src='img/aapje".$i."'.jpg'>";
+}
+?>
+</body>
+</html>
